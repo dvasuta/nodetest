6cbaab50b55aff2cf40b798563ee559ccbd7b252
Dmitry Vasyuta <vst@gala.net>
2012-10-02 02:02:41 +0200

    app added

---
+++ app.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello from <a href="http://appfog.com">AppFog.com</a>');
+}).listen(1337, null);
